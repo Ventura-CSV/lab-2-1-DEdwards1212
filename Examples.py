0e71a61c76e3db5df211a8e5c63d4d3631373f53
David Edwards <david_edwards4@my.vcccd.edu>
2024-06-30 14:45:34 -0700

    examples/notes

---
+++ Examples.py
@@ -0,0 +1,31 @@
+    # Printing Practice
+
+print ("Hello World")
+print ("Howw to print a special character like ' ")
+print (" ' ")
+print (' " ')
+# print (" " ") error
+# print (' ' ') error
+
+"""
+    Multiple line comments
+    This section is a comment box
+    All lines are ignored by Python Interperter
+    """
+print ("Code starts from here")
+    
+    # Variables
+    
+num1 = 10
+print (num1)
+    
+    # Input
+    
+print ('variable = input prompt')
+    
+    #input example
+    
+num = input ('Enter your number')
+print (num)
+sum = int(num) + 100
+print (sum)
